ea1a1c1b36876c557daabb25f858bf1ff08eb091
Foreman76 <alexid1376@gmail.com>
2020-05-11 15:57:59 +0700

    add root.text_title

--- baseclas/modalspinner.py
+++ baseclas/modalspinner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from kivy.uix.modalview import ModalView
+from kivy.properties import StringProperty
 
 class ModalSpinner(ModalView):
-    pass
+    text_title = StringProperty('Загрузка...')
